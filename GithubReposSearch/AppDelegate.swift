692e1e7be5aeb71ded001a5b7f887b5404a78d5f
sandra.morcos <sandra.morcos@itworxedu.com>
2021-05-21 18:13:18 +0200

    Initial Commit

---
+++ GithubReposSearch/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GithubReposSearch
+//
+//  Created by Sandra Morcos on 5/21/21.
+//  Copyright © 2021 Sandra Morcos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'GithubReposSearch' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GithubReposSearch
+  pod 'SwiftLint'
+  pod 'RxSwift'
+  pod 'Kingfisher', '4.10.1'
+  pod 'Moya'
+
+  target 'GithubReposSearchTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
